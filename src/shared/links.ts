162ef13a8135352b3a144e8d317a3d4e8e844e43
dwarjie <macoycoder@gmail.com>
2026-07-18 16:27:18 +0800

    chore: fix logo and links

--- src/shared/links.ts
+++ src/shared/links.ts
@@ -1,4 +1,4 @@
 // External project links shown in the popup/options footer.
 // TODO: point these at the real repo once it's published.
-export const GITHUB_URL = 'https://github.com/dwarjie/stage-keeper'
-export const SUPPORT_URL = 'https://github.com/dwarjie/stage-keeper/issues'
+export const GITHUB_URL = 'https://github.com/dwarjie/stagekeeper';
+export const SUPPORT_URL = 'https://ko-fi.com/dwarjie';
